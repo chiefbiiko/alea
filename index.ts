02b961e833cac7d48097320724db13d64b244234
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2018-11-28 23:24:28 +0100

    og

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
+// Johannes Baagøe <baagoe@baagoe.com>, 2010
 function createMash () : Function {
-  var n = 0xefc8249d
+  var n: number = 0xefc8249d
   return function mash (data: any) : number {
     data = String(data)
-    const len: number = data.length
-    for (var i: number = 0; i < len; i++) {
+    const data_len: number = data.length
+    for (var i: number = 0; i < data_len; i++) {
       n += data.charCodeAt(i)
       let h: number = 0.02519603282416938 * n
       n = h >>> 0
@@ -17,25 +18,27 @@ function createMash () : Function {
   }
 }
 
-export default function alea (seed: any) : Function {
-  var _c: number = 1
-  const _state: { [key:string]: number } = { '0': 0, '1': 0, '2': 0 }
+export default function alea (seed: string|Array<any>) : Function {
+  const seed_len: number = seed.length
   const mash: Function = createMash()
-  _state['0'] = mash(' ')
-  _state['1'] = mash(' ')
-  _state['2'] = mash(' ')
-  _state['0'] -= mash(seed)
-  if (_state['0'] < 0) _state['0'] += 1
-  _state['1'] -= mash(seed)
-  if (_state['1'] < 0) _state['1']+= 1
-  _state['2'] -= mash(seed)
-  if (_state['2'] < 0) _state['2'] += 1
+  var c: number = 1
+  var s0: number = 0
+  var s1: number = 0
+  var s2: number = 0
+  for (var i: number = 0; i < seed_len; i++) {
+    s0 -= mash(seed[i])
+    if (s0 < 0) s0 += 1
+    s1 -= mash(seed[i])
+    if (s1 < 0) s1 += 1
+    s2 -= mash(seed[i])
+    if (s2 < 0) s2 += 1
+  }
   return function next () : number {
-    const t: number = 2091639 * _state['0'] + _c * 2.3283064365386963e-10 // 2^-32
-    _state['0'] = _state['1']
-    _state['1'] = _state['2']
-    _c = t | 0
-    _state['2'] = t - _c
-    return _state['2']
+    const t: number = 2091639 * s0 + c * 2.3283064365386963e-10 // 2^-32
+    s0 = s1
+    s1 = s2
+    c = t | 0
+    s2 = t - c
+    return s2
   }
 }
